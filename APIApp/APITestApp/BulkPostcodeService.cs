f931ea982deb2cdc6e1d503cbde80f9036f5df85
Samuel Burton <sburton@spartaglobal.com>
2023-04-12 17:27:55 +0100

    got some things working for bulk

---
+++ APIApp/APITestApp/BulkPostcodeService.cs
@@ -0,0 +1,61 @@
+﻿using apitestapp;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITestApp
+{
+    public class BulkPostcodeService
+    {
+        #region Properties
+        // Handles comms with the API
+        public RestClient Client { get; set; }
+        public RestResponse Response { get; set; }
+        public JObject ResponseContent { get; set; }
+        public BulkPostcodeResponse ResponseObject { get; set; }
+        #endregion
+
+        public BulkPostcodeService()
+        {
+            Client = new RestClient(AppConfigReader.BaseUrl);
+        }
+
+        public async Task MakeRequestAsync()
+        {
+            var bulkPostcodeRequest = new RestRequest("/postcodes/", Method.Post);
+            bulkPostcodeRequest.AddHeader("Content-Type", "application/json");
+
+            var postcodes = new
+            {
+                Postcodes = new string[] { "OX49 5NU", "M32 0JG", "NE30 1DP" }
+            };
+
+            bulkPostcodeRequest.AddJsonBody(postcodes);
+            RestResponse bulkPostcodeResponse = await Client.ExecuteAsync(bulkPostcodeRequest);
+            ResponseContent = JObject.Parse(bulkPostcodeResponse.Content);
+            ResponseObject = JsonConvert.DeserializeObject<BulkPostcodeResponse>(bulkPostcodeResponse.Content);
+
+
+
+            //var client = new RestClient(options);
+
+            //var bulkPostcodeRequest = new RestRequest("/postcodes/", Method.Post);
+            //bulkPostcodeRequest.AddHeader("Content-Type", "application/json");
+
+            //var postcodes = new
+            //{
+            //    Postcodes = new string[] { "OX49 5NU", "M32 0JG", "NE30 1DP" }
+            //};
+
+
+            ////request.AddStringBody(body, DataFormat.Json);
+            //bulkPostcodeRequest.AddJsonBody(postcodes);
+            //RestResponse bulkPostcodeResponse = await client.ExecuteAsync(bulkPostcodeRequest);
+        }
+    }
+
+
+}
---
+++ APIApp/APITestApp/SinglePostCodeServiceTests/WhenTheBulkPostcodeServiceIsCalled_WithValidPostcode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITestApp.SinglePostCodeServiceTests
+{
+    public class WhenTheBulkPostcodeServiceIsCalled_WithValidPostcode
+    {
+
+        private BulkPostcodeService _bulkPostcodeService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            _bulkPostcodeService = new BulkPostcodeService();
+            _bulkPostcodeService.MakeRequestAsync().GetAwaiter().GetResult();
+        }
+
+        [Test]
+        public void StatusIs200_InJsonResponseBody()
+        {
+            Assert.That(_bulkPostcodeService.ResponseContent["status"].ToString(), Is.EqualTo("200"));
+        }
+
+        [Test]
+        public void StatusIs200_InResponseHeader()
+        {
+            Assert.That((int)_bulkPostcodeService.Response.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void CorrectPostcodeIsReturned()
+        {
+            var result = _bulkPostcodeService.ResponseContent["result"]["postcode"].ToString();
+            Assert.That(result, Is.EqualTo("EC2Y 5AS"));
+        }
+
+        [Test]
+        public void ContentType_IsJson()
+        {
+            Assert.That(_bulkPostcodeService.Response.ContentType, Is.EqualTo("application/json"));
+        }
+
+        [Test]
+        public void ConnectionIsKeepAlive()
+        {
+            var result = _bulkPostcodeService.Response.Headers.Where(x => x.Name == "Connection").Select(x => x.Value).FirstOrDefault();
+            Assert.That(result, Is.EqualTo("keep-alive"));
+        }
+
+        [Test]
+        public void ObjectStatusIs200()
+        {
+            Assert.That(_bulkPostcodeService.ResponseObject.status, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void StatusInResponseHeader_SameAsStatusInResponseBody()
+        {
+            Assert.That((int)_bulkPostcodeService.Response.StatusCode, Is.EqualTo(_bulkPostcodeService.ResponseObject.status));
+        }
+
+        
+        //[Test]
+        //public void AdminDistrict_IsCityOfLondon()
+        //{
+        //    Assert.That(_bulkPostcodeService.ResponseObject.result.admin_district, Is.EqualTo("City of London"));
+        //}
+
+    }
+}
